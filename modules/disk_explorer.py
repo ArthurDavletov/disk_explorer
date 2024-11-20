49d8776c0fe459b2aa5fb711be2f18ffc89cb30f
Артур <ar.davletov2013@gmail.com>
2024-11-20 08:33:40 +0500

    Функционал кнопок вперёд, назад

--- modules/disk_explorer.py
+++ modules/disk_explorer.py
@@ -14,46 +14,75 @@ class DiskExplorer(QMainWindow):
         super(self.__class__, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.night_mode = False
+        self.night_mode: bool = False
+        self.path_history: None | Path = None
         self.__after_init()
 
     def __after_init(self):
         self.ui.change_color_button.clicked.connect(self.__toggle_night_mode)
         self.ui.back_button.clicked.connect(self.__on_back_button_clicked)
+        self.ui.forward_button.clicked.connect(self.__on_forward_button_clicked)
+        self.ui.table_view.doubleClicked.connect(self.__on_cell_clicked)
+
         self.model = MyFSModel()
         self.model.setRootPath(self.model.rootPath())
         self.ui.table_view.setRootIndex(self.model.index(self.model.rootPath()))
         self.ui.table_view.setModel(self.model)
-        self.ui.table_view.doubleClicked.connect(self.__on_cell_clicked)
+
+        self.__resize_header()
+
+    def __resize_header(self):
         header = self.ui.table_view.horizontalHeader()
         header.setSectionResizeMode(0, QHeaderView.Stretch)
-        header.setSectionResizeMode(1, QHeaderView.ResizeToContents)
-        header.setSectionResizeMode(2, QHeaderView.ResizeToContents)
-        header.setSectionResizeMode(3, QHeaderView.ResizeToContents)
-        header.setSectionResizeMode(4, QHeaderView.ResizeToContents)
-        header.setSectionResizeMode(5, QHeaderView.ResizeToContents)
+        for i in range(1, 6):
+            header.setSectionResizeMode(i, QHeaderView.ResizeToContents)
 
     def __on_cell_clicked(self, index):
-        path = self.model.filePath(index)
-        if Path(path).is_dir():
-            self.ui.table_view.setRootIndex(self.model.index(path))
-            self.model.setRootPath(path)
+        path_str = self.model.filePath(index)
+        path = Path(path_str)
+        if path.is_dir():
+            self.ui.table_view.setRootIndex(self.model.index(path_str))
+            self.model.setRootPath(path_str)
+            self.path_history = path
             self.__update_counts(path)
+            self.__update_path_line()
         else:
             os.startfile(path)
 
-    def __update_counts(self, path):
-        for child in Path(path).iterdir():
+    def __update_counts(self, path: Path):
+        for child in path.iterdir():
             self.model.fetch_counts(child)
+
+    def __update_path_line(self):
+        self.ui.path_edit.setText(self.model.rootPath())
 
     def __on_back_button_clicked(self):
         current_index = self.ui.table_view.rootIndex()
         parent_index = self.model.parent(current_index)
         if parent_index.isValid():
             self.ui.table_view.setRootIndex(parent_index)
+            path_str = self.model.filePath(parent_index)
+            self.model.setRootPath(path_str)
+            self.path_history = Path(path_str)
         else:
             self.model.setRootPath("")
             self.ui.table_view.setRootIndex(self.model.index(self.model.rootPath()))
+            self.ui.path_edit.clear()
+            self.path_history = None
+        self.__update_path_line()
+
+    def __on_forward_button_clicked(self):
+        base = Path(self.model.rootPath())
+        target = self.path_history
+        if not target.resolve().is_relative_to(base.resolve()):
+            return
+        print(base, target)
+        base /= target.relative_to(base).parts
+
+        self.ui.table_view.setRootIndex(self.model.index(str(base)))
+        path_str = self.model.filePath(self.model.index(str(base)))
+        self.model.setRootPath(path_str)
+        self.__update_path_line()
 
     def __toggle_night_mode(self):
         # элемент: [светлый, тёмный]
